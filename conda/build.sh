b6abbb1c3155ca5e5e7171e0b85559ec2cf8ad4b
Bastian Schiffthaler <b.schiffthaler@gmail.com>
2020-10-16 10:57:03 +0200

    More work for conda recipe. Better defaults in CMake, and consistent naming

--- conda/build.sh
+++ conda/build.sh
@@ -2,13 +2,6 @@
 
 set -euxo pipefail
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-else
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
-
 mkdir build
 pushd build
 
@@ -19,8 +12,7 @@ CMAKE_PREFIX_PATH=${BUILD_PREFIX} \
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_PLATFORM_FLAGS=$CMAKE_PLATFORM_FLAGS \
-      -DNARROMI_USE_CLP=ON \
-      -DSEIDR_PSTL=ON ..
+      ..
 
 make -j${CPU_COUNT}
 make install
